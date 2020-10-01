89ea7e1384af69b09c745385c38ba182a8942b60
ARKobelkov <arkobelkov@ya.ru>
2020-10-02 02:01:51 +0300

    доделал шапку (кроме выпадающего списка)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,30 @@
 "use strict";
 
-var name = "\u0418\u043C\u044F";
+$(function () {
+  /* Поддержка svg спрайтов в IE 11 */
+  svg4everybody(); // Main-slider
 
-var func = function func() {
-  console.log(123);
-};
+  $('#main-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: false,
+    fade: true,
+    speed: 1000,
+    pauseOnHover: false
+  }); // Descr-slider
+
+  $('#descr-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: true,
+    speed: 1000,
+    appendDots: '#descr-slider'
+  });
+});
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
-let name = `Имя`;
+$(function () {
+	/* Поддержка svg спрайтов в IE 11 */
+	svg4everybody();
 
-let func = () => {
-	console.log(123);
-};
+	// Main-slider
+	$('#main-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		dots: false,
+		fade: true,
+		speed: 1000,
+		pauseOnHover: false
+	});
+
+	// Descr-slider
+	$('#descr-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		autoplaySpeed: 5000,
+		arrows: false,
+		dots: true,
+		speed: 1000,
+		appendDots: '#descr-slider'
+	});
+
+});
